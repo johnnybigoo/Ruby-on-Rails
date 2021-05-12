5f3b965d9b8f73051967e6245df99c04163fbf0b
johnnybigoo <jotalico@icloud.com>
2021-05-11 21:13:15 -0400

    8-preparando-os-controllers

--- OneBitFood/app/controllers/restaurants_controller.rb
+++ OneBitFood/app/controllers/restaurants_controller.rb
@@ -1,7 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
+		@restaurants = Restaurant.all
+		before_action :set_restaurant, only: :show
   end
 
   def show
   end
+
+	private
+
+	def set_restaurant
+		@restaurant = Restaurant.find(params[:id])
+	end
 end

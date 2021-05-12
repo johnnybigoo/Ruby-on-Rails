88fa5d68025853901544c3519699b052819feb32
johnnybigoo <jotalico@icloud.com>
2021-05-11 22:34:39 -0400

    10-Controller-order-fixed

--- OneBitFood/app/views/orders/_order.json.jbuilder
+++ OneBitFood/app/views/orders/_order.json.jbuilder
@@ -2,4 +2,8 @@ json.id order.id
 json.restaurant_id order.restaurant_id
 json.total_value order.total_value
 json.status order.status
-json.address order.address
+json.city order.city
+json.street order.street
+json.neighbohood.neighborhood
+json.number order.number
+json.complement order.complement
--- OneBitFood/db/schema.rb
+++ OneBitFood/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_031347) do
+ActiveRecord::Schema.define(version: 2021_05_12_020654) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_05_11_031347) do
     t.integer "restaurtant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "street"
     t.index ["restaurtant_id"], name: "index_orders_on_restaurtant_id"
   end
 

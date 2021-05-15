679e1e1acc839154e10c5b5be86df8b0152f8595
johnnybigoo <jotalico@icloud.com>
2021-05-14 23:30:30 -0400

    12-Baixando-as-Categorias

--- onebitfood-client/services/getCategories.js
+++ onebitfood-client/services/getCategories.js
@@ -1,12 +1,28 @@
 import useSWR from 'swr';
+import { useRouter } from 'next/router';
+// import { useRecoilState } from 'recoil';
+// import addressState from '../store/atoms/addressAtom';
 
-export default function getCategories() {
+export default function getRestaurants() {
+	const router = useRouter();
+	const { category, q } = router.query;
+	// const [address] = useRecoilState(addressState)
+
+	let params = '';
+	if (category)
+		params = `${params == '' ? '?' : '&'}category=${category}`
+	if (q)
+		params = `${params == '' ? '?' : '&'}q=${q}`
+	if (address.city != '')
+		params = `${params == '' ? '?' : '&'}city=${address.city}`
+
 	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 	const { data, error } = useSWR(
-		`${process.env.apiUrl}/api/categories`,
-		fetcher, { revalidateOnFocus: false }
+		`${process.env.apiUrl}/api/restaurants${params}`,
+		fetcher,
+		{ revalidateOnFocus: false }
 	)
 
-	return { categories: data, isLoading: !error && !data, isError: error }
+	return { restaurants: data, isLoading: !error && !data, isError: error }
 }

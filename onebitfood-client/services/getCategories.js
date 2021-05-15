c8d2066ad95ba1b57bf98aa2d8b8c7f2bec58d6e
johnnybigoo <jotalico@icloud.com>
2021-05-14 23:42:42 -0400

    13-Criando-Busca-2

--- onebitfood-client/services/getCategories.js
+++ onebitfood-client/services/getCategories.js
@@ -1,12 +1,12 @@
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
-// import { useRecoilState } from 'recoil';
-// import addressState from '../store/atoms/addressAtom';
+import { useRecoilState } from 'recoil';
+import addressState from '../store/atoms/addressAtom';
 
 export default function getRestaurants() {
 	const router = useRouter();
 	const { category, q } = router.query;
-	// const [address] = useRecoilState(addressState)
+	const [address] = useRecoilState(addressState)
 
 	let params = '';
 	if (category)
